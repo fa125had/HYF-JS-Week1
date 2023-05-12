8fbeec3420d93eec3fc11f89298024457bf8b737
farshad <far5had@outlook.com>
2023-05-12 19:30:50 +0200

    new test style, should be cleaner

--- main.js
+++ main.js
@@ -7,23 +7,43 @@ const generateRandomColor = (alpha = 1.0) => {
   const newRandomColor = `rgba(${randomRed}, ${randomGreen}, ${randomBlue} , ${alpha})`;
   return newRandomColor;
 }
-const hoverHandler = () => {
+const styleChanger = () => {
   const gradientAngle = Math.random() * 360;
-  const fontSize = Math.floor(Math.random() * (85 - 60 + 1) + 30);
+  const fontSize = Math.floor(Math.random() * (85 - 60) + 30);
   headerTitle.style.backgroundImage = `linear-gradient(${gradientAngle}deg , ${generateRandomColor()} , ${generateRandomColor()} , ${generateRandomColor()})`;
   headerTitle.style.border = `${fontSize / 20}px solid ${generateRandomColor()}`;
   headerTitle.style.borderRadius = `${fontSize / 4}px`;
   headerTitle.style.textShadow = ` 0 0  ${fontSize / 1.5}px ${generateRandomColor()}`;
-  // headerTitle.style.fontSize = `${fontSize}px`;
+  headerTitle.style.fontSize = `${fontSize}px`;
   headerTitle.parentElement.style.backgroundColor = `${generateRandomColor(Math.random() * 0.7)}`;
   headerTitle.parentElement.style.borderRadius = `${fontSize / 4}px`;
 
 }
+let intervalId;
+const clickHandler = () => {
+  intervalId = setInterval(() => {
+    styleChanger();
+  }, 100);
+};
 
-document.getElementById('mainBtn').addEventListener('click', () => {
-  for (let i = 0; i < 20000; i++) {
-    setTimeout(() => {
-      hoverHandler();
-    }, 100);
+const stopChangingStyles = () => {
+  clearInterval(intervalId);
+  if (newStyleButton.parentElement.querySelector('#pauseBtn')) {
+    const pauseButton = newStyleButton.parentElement.querySelector('#pauseBtn');
+    pauseButton.remove();
   }
+};
+const newStyleButton = document.getElementById('mainBtn');
+newStyleButton.addEventListener('click', () => {
+  clickHandler();
+  if (!newStyleButton.parentElement.querySelector('#pauseBtn')) {
+    const newButton = document.createElement('button');
+    newStyleButton.parentElement.appendChild(newButton);
+    newButton.className = 'button',
+      newButton.id = 'pauseBtn',
+      newButton.textContent = 'Pause ||',
+      newButton.onclick = stopChangingStyles;
+  }
+
 });
+
